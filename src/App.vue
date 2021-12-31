b24205db8538c8e8344f2a519d9727b3a28ea0f1
Sahil <smppatel99999@gmail.com>
2021-12-31 12:20:59 +0530

    Initial Commit after cleaning Vue Default files and adding axios

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Simple Vue App
+  <Tracker />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Tracker from '@/components/Tracker.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tracker
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Tracker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    I am a Tracker Map
+  </div>
+</template>
+
+<script>
+//import axios from 'axios';
+export default {
+  name: 'TrackerMap',
+  created(){
+    console.log("This is created.")
+    console.log(window.location.hostname)
+    this.axios.get("https://api.ipify.org/?format=json").then(res=>{
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
