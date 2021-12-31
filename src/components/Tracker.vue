45e6f3967417e1fabb8935f4730009d25f0be5f9
Sahil <smppatel99999@gmail.com>
2021-12-31 18:21:37 +0530

    Information Fetch Finished

--- src/components/Tracker.vue
+++ src/components/Tracker.vue
@@ -4,8 +4,9 @@
       <div class="row justify-content-center banner">
         <div class="col-md-8 col-sm-6">
           <form @submit.prevent="sendInfo">
-              <input type="text" class="textField" name="ipinput" placeholder="IP Address" v-model="ipAddress" />
+              <input type="text" class="textField" name="ipinput" placeholder="IP Address" v-model="ipInput" />
               <input type="submit" class="buttonSend" name="sendInfo" />
+              <p align="center" v-if="error" style="color:green;">Invalid IP Adress.</p>
           </form>
         </div>
       </div>
@@ -38,8 +39,52 @@
           </article>
         </div>
 
-        <div class="col-md-6 col-sm-6 bg-primary">
-            
+        <div class="col-md-6 col-sm-6" v-if="ipData">
+            <div style="height: 60vh; width: 100%;">
+                <l-map
+                  v-model="zoom"
+                  v-model:zoom="zoom"
+                  :center="[lat, lang]"
+                  @move="log('move')"
+                >
+                  <l-tile-layer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  ></l-tile-layer>
+
+                  <l-control-layers />
+                  
+                  <!-- <l-marker :lat-lng="[0, 0]" draggable @moveend="log('moveend')">
+                    <l-tooltip>
+                      lollllllll
+                    </l-tooltip>
+                  </l-marker> -->
+
+                
+
+                  <l-marker :lat-lng="[lat, lang]" draggable @moveend="log('moveend')">
+                    <l-tooltip>
+                      {{ipData.region}}
+                    </l-tooltip>
+                  </l-marker>
+
+                  
+
+                 
+                  <l-rectangle
+                    :bounds="[
+                      [46.334852, -1.190568],
+                      [46.241487, -1.090357],
+                    ]"
+                  >
+                    <l-popup>
+                      lol
+                    </l-popup>
+                  </l-rectangle>
+
+                </l-map>
+              </div>
+
+
         </div>
 
       </div>
@@ -52,63 +97,118 @@
     </div>
 
 
-
-<!--   <div class="hello" v-if="ipAddress">
-    I am a Tracker Map for the IP {{ipAddress}}
-    <div class="" v-if="ipData">
-    {{ipData}}  
-    </div>
-  </div> -->
-<!--   <div v-else>
-    Somethign is Wrong
-  </div> -->
-
 </template>
 
 <script>
+
+import "leaflet/dist/leaflet.css"
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+  LTooltip,
+  LPopup,
+  LRectangle,
+} from "@vue-leaflet/vue-leaflet";
+
 import axios from 'axios';
 export default {
   name: 'TrackerMap',
+  components: {
+        LMap,
+        LTileLayer,
+        LMarker,
+        LControlLayers,
+        LTooltip,
+        LPopup,
+        LRectangle,
+  },
   data(){
       return{
           ipAddress: null,
           ipData: null,
-          errorReceived: null
+          errorReceived: null,
+          ipInput: null,
+          zoom: 6,
+          iconWidth: 25,
+          iconHeight: 40,
+          lat: null,
+          lang: null,
+          error:false
+
       };
   },
-  methods: {
-      getIpAddressData(ip){
-          var ipapi = "https://ipapi.co/"+ip+"/json/"
-          axios.get(ipapi).then((res)=>{
-            
-            console.log(res.data)
-            this.ipData = res.data
-
-          }).catch((err)=>{
-            console.log("There is an error fetching ipdata.", err)
-            return false
-          })
-      }
-  },
   created(){
-    console.log("This is created.")
-    
-      var api = "https://api.ipify.org/?format=json"
+          
+          console.log("This is created.")
+          //fetching network IP using API
+          var api = "https://api.ipify.org/?format=json"
+          
           axios.get(api).then((response) => {
+            
+            //setting up the ip to the varible
             var ip = response.data.ip
             this.ipAddress = ip
+            this.ipInput = ip
+
+            //passing the IP to get the information from another API
             this.getIpAddressData(ip)
-            console.log(ip)
-
-
-
+            
 
           }).catch((err)=>{
+            
             console.log("Error IPDATA FETCH", err)
+          
           })
 
 
-  }
+  },
+  computed: {
+      iconSize() {
+        return [this.iconWidth, this.iconHeight];
+      },
+    },
+  methods: {
+      sendInfo(){
+        this.getIpAddressData(this.ipInput)
+        console.log("Called ", this.ipAddress)
+      },
+      getIpAddressData(ip){
+          
+          var ipapi = "https://ipapi.co/"+ip+"/json/"
+          
+          //fetching IP info using API
+          axios.get(ipapi).then((res)=>{
+            
+            //logging the info data to console  
+            console.log(res.data)
+            if(res.data.error){
+              this.error = res.data.error
+            }else{
+              //setting up the data to variables
+              this.ipData = res.data
+              this.lat = res.data.latitude
+              this.lang = res.data.longitude
+              return true              
+            }
+
+          }).catch((err)=>{
+
+            console.log("There is an error fetching ipdata.", err)
+            
+            return false
+          })
+      },
+
+      log(a) {
+            console.log(a);
+          },
+
+      
+
+  },
+  
 }
 </script>
 
